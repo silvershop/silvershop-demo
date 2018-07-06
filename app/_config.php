0c2dfddb5c9b8421170a1f9bd8cba8aebdffe07b
Will Rossiter <will@fullscreen.io>
2018-07-06 14:43:59 +1200

    Clear database on script run, run populate task

--- app/_config.php
+++ app/_config.php
@@ -3,6 +3,7 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
+use SilverStripe\Core\Environment;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
@@ -11,5 +12,7 @@ $validator->minLength(8);
 $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
 
+global $database;
+$database = ($name = Environment::getEnv('SS_DATABASE_NAME')) ? $name : 'silvershopcore';
 
 if (Director::isLive()) Director::forceSSL();
