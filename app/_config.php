935967cedab9464dde1d3f3370b6d02ea8cb93d4
Will Rossiter <will@fullscreen.io>
2018-07-06 14:20:07 +1200

    Force ssl

--- app/_config.php
+++ app/_config.php
@@ -2,6 +2,7 @@
 
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
+use SilverStripe\Control\Director;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
@@ -9,3 +10,6 @@ $validator = new PasswordValidator();
 $validator->minLength(8);
 $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
+
+
+if (Director::isLive()) Director::forceSSL();
