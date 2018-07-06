d40118cea0a4ea004d204d55ea7bed660c9f86fb
Will Rossiter <will@fullscreen.io>
2018-07-06 14:56:39 +1200

    cli-script fails in a docker container, replace

---
+++ app/code/cli-script-docker-fixed.php
@@ -0,0 +1,27 @@
+<?php
+
+// CLI specific bootstrapping
+use SilverStripe\Control\CLIRequestBuilder;
+use SilverStripe\Control\HTTPApplication;
+use SilverStripe\Core\CoreKernel;
+
+require dirname(dirname(__DIR__)). '/vendor/silverstripe/framework/src/includes/autoload.php';
+
+// Ensure that people can't access this from a web-server
+if (!in_array(PHP_SAPI, ["cli", "cgi", "cgi-fcgi"])) {
+    echo "cli-script.php can't be run from a web request, you have to run it on the command-line.";
+    die();
+}
+
+// Build request and detect flush
+$request = CLIRequestBuilder::createFromEnvironment();
+
+// Default application
+$kernel = new CoreKernel(BASE_PATH);
+
+// required for whatever reason to generate output
+echo PHP_EOL;
+
+$app = new HTTPApplication($kernel);
+$response = $app->handle($request);
+$response->output();
