5d4ac2f119f660c44125f3eea35ce1f6063f839a
Will Rossiter <will@fullscreen.io>
2019-11-14 11:39:51 +1300

    Add banner with username / password

---
+++ app/js/adminNotification.js
@@ -0,0 +1,7 @@
+(function($) {
+	$(document).ready(function() {
+		$('body').prepend(
+			$('<div class="admin-note"><p>You are viewing a demo of SilverShop. To login visit <a href="http://demo.silvershop.io/admin">demo.silvershop.io/admin</a>. Username and password is silvershopcore.</p></div>')
+		);
+	})
+})(jQuery);
