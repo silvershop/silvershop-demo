7e03b147f88ce6e225383bd8c560f0768ed53424
Will Rossiter <will@fullscreen.io>
2026-05-06 09:36:02 +1200

    fix: url

--- app/js/adminNotification.js
+++ app/js/adminNotification.js
@@ -2,7 +2,7 @@
   $(document).ready(function () {
     $("body").prepend(
       $(
-        '<div class="admin-note"><p>You are viewing a demo of SilverShop. To login visit <a href="https://silvershop-demo.fullscreen.io/admin">admin</a>. Username and password is silvershopcore.</p></div>',
+        '<div class="admin-note"><p>You are viewing a demo of SilverShop. To login visit <a href="https://silvershop.fullscreen.io/admin">admin</a>. Username and password is silvershopcore.</p></div>',
       ),
     );
   });
