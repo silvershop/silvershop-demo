f54a9a3649bb00f3c4df421aa2fc2634a4fed1b3
Will Rossiter <will@fullscreen.io>
2026-05-10 19:04:01 +1200

    feat: remove jquery requirement

--- app/js/adminNotification.js
+++ app/js/adminNotification.js
@@ -1,9 +1,7 @@
-(function ($) {
-  $(document).ready(function () {
-    $("body").prepend(
-      $(
-        '<div class="admin-note"><p>You are viewing a demo of SilverShop. To login visit <a href="https://silvershop.fullscreen.io/admin">admin</a>. Username and password is silvershopcore.</p></div>',
-      ),
-    );
-  });
-})(jQuery);
+document.addEventListener("DOMContentLoaded", function () {
+  var note = document.createElement("div");
+  note.className = "admin-note";
+  note.innerHTML =
+    '<p>You are viewing a demo of SilverShop. To login visit <a href="http://demo.silvershop.io/admin">demo.silvershop.io/admin</a>. Username and password is silvershopcore.</p>';
+  document.body.prepend(note);
+});
